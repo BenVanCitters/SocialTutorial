6ff51f59b86c1683ead383ae8bd61b051629cd23
Ben V <van.citters.b@gmail.com>
2020-08-21 18:06:19 -0500

    https://www.youtube.com/watch?v=86LQTtxygWU second part

--- src/bffbook/urls.py
+++ src/bffbook/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import  include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home_view
@@ -22,6 +23,8 @@ from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view, name='home-view'),
+    # path('profiles/', include('profiles.url'), na)
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
