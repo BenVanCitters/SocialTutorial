a12713a5097a6e3ef5146d861993ff1230330952
Ben V <van.citters.b@gmail.com>
2020-08-24 02:04:50 -0500

    https://www.youtube.com/watch?v=orTvLAysEHQ whole video

--- src/bffbook/urls.py
+++ src/bffbook/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('',home_view, name='home-view'),
     # path('profiles/', include('profiles.url'), na)
     path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
